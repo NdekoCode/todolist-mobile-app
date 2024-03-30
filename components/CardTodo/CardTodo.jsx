73a2e7d5679b006aafe500ab86dbfd14cf5dd3c4
Ndekocode <arickbulakali@gmail.com>
2024-03-30 12:38:08 +0200

    refactor: replace TouchableHighlight by TouchableOpacity in TodoCard

--- components/CardTodo/CardTodo.jsx
+++ components/CardTodo/CardTodo.jsx
@@ -1,16 +1,16 @@
-import { Image, Text, TouchableHighlight, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import check from "../../assets/check.png";
 import { s } from "./cardTodo.style";
 const CardTodo = ({ todo }) => {
   return (
-    <TouchableHighlight style={s.app}>
+    <TouchableOpacity style={s.app}>
       <>
         <Text>{todo.title}</Text>
         <View style={s.imgContainer}>
           {todo.isCompleted && <Image source={check} style={s.img} />}
         </View>
       </>
-    </TouchableHighlight>
+    </TouchableOpacity>
   );
 };
 
