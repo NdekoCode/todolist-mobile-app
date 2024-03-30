b540fd9d8ed31f7ffa92eaeae66ef28ea64839bd
Ndekocode <arickbulakali@gmail.com>
2024-03-30 15:07:57 +0200

    feat: add todos reducer fonctions

--- data/constants.jsx
+++ data/constants.jsx
@@ -14,4 +14,34 @@ const TODO_LIST = [
   { id: 13, title: "Apprendre Next.js14", isCompleted: true },
 ];
 const TABS = ["All", "In Progress", "Done"];
-export { TODO_LIST,TABS };
+const filterTodos = (state,filter)=>{
+    switch(filter){
+        case "DONE":
+        return state.filter(t=>t.isCompleted);
+        case "IN_PROGRESS":
+            return state.filter(t=>!t.isCompleted);
+        default:
+            return state
+    }
+}
+const todosReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return [
+        ...state,
+        { id: Date.now(), title: action.payload, isCompleted: false },
+      ];
+    case "DELETE_TODO":
+      return state.filter(todo.id !== action.payload);
+    case "TOGGLE_TODO":{
+      const todoIndex = state.findIndex((t) => t.id === action.payload);
+      state[todoIndex].isCompleted = !state[todoIndex].isCompleted;
+      return state;
+    }
+    case "FILTER_TODO":
+        return filterTodos(state,action.payload);
+    default:
+      return state;
+  }
+};
+export { TODO_LIST, TABS };
