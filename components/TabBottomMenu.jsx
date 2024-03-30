a31a274d950504e7d534a2094a1d5409d6f294f5
Ndekocode <arickbulakali@gmail.com>
2024-03-30 15:57:28 +0200

    refactor: change count todos logic by using reduce function

--- components/TabBottomMenu.jsx
+++ components/TabBottomMenu.jsx
@@ -2,12 +2,16 @@ import { Text, TouchableOpacity } from "react-native";
 import { TABS } from "../data/constants";
 import { appStyle } from "../styles/app.style";
 
-const TabBottomMenu = ({ activeIndex, handlePress,todos }) => {
-
-  const allTodo = todos.length;
-  const inProgressTodoCount = todos.filter((t) => !t.isCompleted).length;
-  const doneTodoCount = todos.filter((t) => t.isCompleted).length;
-  const todoState = [allTodo, inProgressTodoCount, doneTodoCount];
+const TabBottomMenu = ({ activeIndex, handlePress, todos }) => {
+    // todos.reduce(functionAccumulator(acc,todo),initialValue); =>// functionAccumulator(laValeurCourrante,laProchaineValeur,indexDansLeTableau)
+  const todoStatusCount = todos.reduce(
+    (acc, todo) => {
+    todo.isCompleted ? acc.done++ : acc.inProgress++;
+      return acc;
+    },
+    { all: todos.length, inProgress: 0, done: 0 }
+  );
+  const todoState = [todoStatusCount.all, todoStatusCount.inProgress, todoStatusCount.done];
   const activeStyle = { color: "#606fda" };
   return (
     <>
